17fb17b6fcc1f12a6a2334f999da3a422e26f463
Hayden Ross <Ross.r.hayden@gmail.com>
2025-01-13 19:52:04 +0000

    New File M2.4

---
+++ CraftPricing.java
@@ -0,0 +1,12 @@
+/**
+ * Hayden Ross
+ * M2.4 Programming Assignment 2
+ * intake name, material cost, and hours to build
+ * using this information calulate retail price 
+ * calculate retail prive based upon discount 
+ */
+
+public class CraftPricing {
+    
+    
+}
