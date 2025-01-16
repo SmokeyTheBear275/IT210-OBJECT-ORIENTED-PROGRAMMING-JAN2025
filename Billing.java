58be28c8cf2043f6b9aa48fbf3008674871e1ea7
Hayden Ross <Ross.r.hayden@gmail.com>
2025-01-15 21:30:54 -0500

    Update Billing.java

--- Billing.java
+++ Billing.java
@@ -1,1 +1,36 @@
 
+/**
+ * Hayden Ross
+ * M3.4 Programming Assignment 2
+ * Create four overloaded methods for computing price
+ * 
+ * I set the numbers in all the math to variables so they can be easily changed like discount percentage
+ * I also added error checks to input because I was curious how to do it
+ */
+
+public class CraftPricing
+{
+    public static void main(String[] args)
+    {
+        //define variables
+        double price
+        double quantity
+        double couponValue
+        double weeklyDiscount
+
+        //create scanner
+        Scanner keyboard = new Scanner(System.in);
+
+        //get price and determine it is a double / integer then re-ask for proper input
+        while (true) {
+            System.out.println("Enter book cost: ");
+            try {
+                price = Double.parseDouble(keyboard.nextLine());
+                break;
+            }
+            catch (NumberFormatException ignore) {
+                System.out.println("Error not a price");
+            }
+          }
+
+  
