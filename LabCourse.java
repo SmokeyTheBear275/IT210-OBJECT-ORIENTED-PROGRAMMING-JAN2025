dd22e9437c3fd485b8fc401dc1891a7291f02663
SmokeyTheBear275 <ross.r.hayden@gmail.com>
2025-02-23 21:19:49 -0500

    Working code

--- LabCourse.java
+++ LabCourse.java
@@ -6,7 +6,7 @@
  * Establish fee based upon if lab required
  */
 
-//Labcourse is set as a child of CollegeCourse
+//LabCourse is set as a child of CollegeCourse
 public class LabCourse extends CollegeCourse{
 
     //Reference parent constructor and adjust the fee for the lab
@@ -18,7 +18,6 @@ public class LabCourse extends CollegeCourse{
     //Override the display function if it is a lab course and display the accurate data
     @Override
     public void display() {
-        super.display();
         System.out.println("\n Department: " + dept);
         System.out.println("\n Course Number: " + courseNum);
         System.out.println("\n Credits: " + credits);
